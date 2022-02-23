0128cfd7153304618407169c17ba0ace404a36d5
Djey013 <jeremy.vernagallo@hotmail.fr>
2022-02-23 22:21:27 +0100

    introduction mouse input
    A terminer

--- Assets/Tank.cs
+++ Assets/Tank.cs
@@ -7,35 +7,40 @@ public class Tank : BaseController
     public Bullet _bullet;
     public float speed = 0;
     public float speedRotation = 0;
-    
-
-    void Start()
-    {
-        
-    } 
+  
 
     void Update()
     {
         
-        float direction = Input.GetAxis("Vertical") * speed;
+        float direction = Input.GetAxis("Vertical") * speed;            //controle du tank
         transform.Translate(0f,0f,direction*Time.deltaTime,Space.Self);
         
         float rotation = Input.GetAxis("Horizontal") * speedRotation;
         transform.Rotate(0, rotation, 0);
 
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetAxis("Mouse X") > 0)            //
+        {
+            Debug.Log("Mous rotating in X axis");
+        }
+
+
+        if (Input.GetMouseButtonDown(0))            //tir bullet avec LMB
         {
             _bullet.Fire();
+            Debug.Log("Fire !");
         }
+       
+        
     }
     
+
     private void GetMouseDirection() 
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit))
         {
-            
+            Debug.Log("screen point");
         }
     }
     
@@ -57,6 +62,15 @@ void Update()
         }
 
     }
+
+
+-----------------------------------------------------------------------------------------------
+if (Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed secondary button.");
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle click.");
+
 
 
 */
